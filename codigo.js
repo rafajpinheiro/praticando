1d4ca72e3cfb5c592432816e31460ccba52b3661
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-10 23:02:28 -0300

    deletando o arquivo

--- codigo.js
+++
@@ -1,6 +0,0 @@
-let nome = 'PH'
-let sobrenome = 'Ganso'
-let timeAdversario = 'América-MG'
-let msg = `O princpal destaque do jogo contra ${timeAdversario} no Maracanã na tarde de domingo foi ${nome} ${sobrenome}.`
-
-console.log(msg);
