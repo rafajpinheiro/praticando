18bd22704127b28cd683c0b7adc93ed852b9f051
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-10 22:44:02 -0300

    estudando tamplate string

---
+++ codigo.js
@@ -0,0 +1,6 @@
+let nome = 'PH'
+let sobrenome = 'Ganso'
+let timeAdversario = 'América-MG'
+let msg = `O princpal destaque do jogo contra ${timeAdversario} no Maracanã na tarde de domingo foi ${nome} ${sobrenome}.`
+
+console.log(msg);
