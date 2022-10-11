fba19d20eb1e50289aff2bede6921c8a7c11fdfe
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-11 10:50:25 -0300

    praticando template string

--- codigo.js
+++ codigo.js
@@ -4,3 +4,9 @@ let timeAdversario = 'América-MG'
 let msg = `O princpal destaque do jogo contra ${timeAdversario} no Maracanã na tarde de domingo foi ${nome} ${sobrenome}.`
 
 console.log(msg);
+
+let preco = 4.85
+let quantidade = 8
+let valorTotal = `O valor total a ser pago pela sua compra é ${preco*quantidade}.`
+
+console.log(valorTotal);
