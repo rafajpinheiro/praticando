174fc212bcaf06d3d5695a301cc4c6251c090352
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-12 19:23:45 -0300

    atibuindo uma função a uma vairável

--- pratica-function.js
+++ pratica-function.js
@@ -14,3 +14,12 @@ function soma(n1, n2){
 
 soma(10,5);
 soma("Rio de ","Janeiro");
+
+
+function multiplicacao(x,y){
+    return x * y;
+}
+
+let resultado = multiplicacao(5,10);
+
+console.log(resultado);
