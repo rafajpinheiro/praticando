1b0bf57910f45905316dedd5fe4093e2da00a61b
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-12 19:11:22 -0300

    praticando function

---
+++ pratica-function.js
@@ -0,0 +1,16 @@
+let nome = "Rafael"
+let sobrenome = "Pinheiro"
+
+function exibirNome(){
+    console.log(nome + " " + sobrenome);
+}
+
+exibirNome();
+
+
+function soma(n1, n2){
+    console.log(n1 + n2);
+}
+
+soma(10,5);
+soma("Rio de ","Janeiro");
