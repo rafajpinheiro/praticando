6326598cd8f3ddf757d41b61c3b8a1334b53a57e
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-17 22:30:23 -0300

    exibindo a saudação conforme a hora do dia

--- semana-2-ex-1.js
+++ semana-2-ex-1.js
@@ -1,3 +1,4 @@
+/*
 function msg(turno){
 
     switch (turno) {
@@ -14,3 +15,20 @@ function msg(turno){
 }
 
 msg("noite");
+*/
+
+
+let saudacao = (hora) =>{
+    const d = new Date();
+    hora = d.getHours();
+    
+    if (hora >= 6 && hora <=11){
+        console.log("Bom dia!");
+    } else if (hora >= 12 && hora < 18){
+        console.log("Boa tarde!");
+    } else{
+        console.log("Boa noite!");
+    }
+}
+
+saudacao();
