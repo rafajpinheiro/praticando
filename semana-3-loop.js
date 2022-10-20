c8c87082d6df844899d62dc1b8a02a7cdb3e2039
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-10-19 22:34:40 -0300

    estudando loop

---
+++ semana-3-loop.js
@@ -0,0 +1,31 @@
+/*
+let n = 0;
+let x = 0;
+
+let testeLoop = () => {
+    while(n<3){
+        n++;
+        x += n;
+    }
+
+    console.log(n,x)
+}
+
+testeLoop();
+*/
+
+/*
+let salario = 1000;
+
+while (salario < 5000) {
+    salario += 100;
+    console.log("O salário ainda é R$ " + salario);
+}
+*/
+
+let contador = 0;
+
+while (contador <= 50) {
+    console.log("repetição nº " + contador);
+    contador++;
+}
