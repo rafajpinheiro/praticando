d3400e6baeea6569d89d920c4a0c662e0b3aae86
Rafael Pinheiro <rafaeljpinheiro@gmail.com>
2022-11-12 21:47:35 -0300

    primeira lista de exercícios

--- leetcode/pratica-01.js
+++ leetcode/pratica-01.js
@@ -2,9 +2,13 @@ let matriz = [2,5,15,5,3,9,32,11,17,1];
 
 function soma(lista, resultado) {
     
+    //for (i=0; i<lista.length; i++)
+    //if (lista[i] + lista[i+1] == resultado)
+    //console.log(lista[i], lista[i+1]);
+
     for (i=0; i<lista.length; i++)
     if (lista[i] + lista[i+1] == resultado)
     console.log(lista[i], lista[i+1]);
 }
 
-soma(matriz,18);
+soma(matriz,12);
---
+++ udemy-js/ex-01.js
@@ -0,0 +1,6 @@
+//console.log("O arquivo está funcionando.");
+
+const nome = "Rafael";
+const idade = 43;
+
+console.log(`Olá, me chamo ${nome}, tenho ${idade} anos e estou aprendendo Javascript.`)
